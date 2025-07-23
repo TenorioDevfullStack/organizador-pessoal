5f9ef5f3bd5f25509a7c60bd46ca636f10da46ff
Leandro Tenorio da Silva <tenorioleandro22@gmail.com>
2025-07-23 14:28:09 -0300

    Remove service worker customizado e registro manual para compatibilidade com Vercel/CRA

--- src/index.js
+++ src/index.js
@@ -10,15 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-// Registrar o service worker para PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('ServiceWorker registrado com sucesso:', registration);
-      })
-      .catch(error => {
-        console.log('Falha ao registrar o ServiceWorker:', error);
-      });
-  });
-}
